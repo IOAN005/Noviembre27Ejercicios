cee0c7244d6b6f7a7f5ee3fb165876a85e50c83e
IOAN005 <nellbovinca@gmail.com>
2023-11-27 19:48:41 +0100

    Initial commit

---
+++ src/com/corenetworks/presentacion/ProbarMonenedasejs1.java
@@ -0,0 +1,10 @@
+package com.corenetworks.presentacion;
+
+public class Monedasejs1 {
+    public static void main(String[] args){
+        Monedasejs1 objmoneda =new Monedasejs1();
+        System.out.println(objmoneda.ca);
+
+
+    }
+}
